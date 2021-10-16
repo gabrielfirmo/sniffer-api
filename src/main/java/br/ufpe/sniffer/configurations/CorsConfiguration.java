53cbd400db2d5e51f6211ec1013c07ce2ef4cc0a
gabrielfirmo <gabriel_firmo@hotmail.com>
2021-10-16 19:02:03 -0300

    Cors

--- src/main/java/br/ufpe/sniffer/configurations/CorsConfiguration.java
+++ src/main/java/br/ufpe/sniffer/configurations/CorsConfiguration.java
@@ -2,15 +2,20 @@ package br.ufpe.sniffer.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .maxAge(-1)   // add maxAge
+                .allowCredentials(false);
     }
 }
